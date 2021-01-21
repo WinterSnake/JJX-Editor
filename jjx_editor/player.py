62b6db7ab23371b6071599f384d6800545773382
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-01-21 02:06:09 -0500

    feat: gender, hair color, difficulty enum
    update(player): use enum types

--- jjx_editor/player.py
+++ jjx_editor/player.py
@@ -7,17 +7,55 @@
 
 ## Imports
 from __future__ import annotations
+from enum import Enum
 
 
 ## Classes
+class Gender(Enum):
+    """Gender of player"""
+    Male = 0
+    Female = 1
+
+
+class HairColor(Enum):
+    """Hair color of player"""
+    White = 0x00
+    Gray = 0x10
+    Black = 0x20
+    Brown = 0x30
+    Dark_Brown = 0x40
+    Light_Brown = 0x50
+    Blonde = 0x60
+    Dirty_Blonde = 0x70
+    Bleach_Blonde = 0x80
+    Orange = 0x90
+    Scarlet = 0xA0
+    Purple = 0xB0
+    Navy = 0xC0
+    Teal = 0xD0
+    Green = 0xE0
+    Yellow = 0xF0
+
+
+class Difficulty(Enum):
+    """Local difficulty of player"""
+    Peaceful = 0
+    Easy = 1
+    Normal = 2
+    Hard = 3
+    Very_Hard = 4
+
+
 class Player:
     """"""
 
     # -Constructor
     def __init__(
-        self, name: str, gender: bool, skin: int,
-        hair_style: int, hair_color: int, difficulty: int,
-        permadeath: bool, simple_craft: bool, perpetual_time: bool
+        self, name: str, gender: Gender, skin: int,
+        hair_style: int, hair_color: HairColor,
+        difficulty: Difficulty = Difficulty.Normal,
+        permadeath: bool = False, simple_craft: bool = False,
+        perpetual_time: bool = False
     ) -> Player:
         ''''''
         # -Character
