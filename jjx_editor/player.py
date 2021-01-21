8b4c5df8ae6acb91dd47331ba6b5f3dc1b20df3c
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-01-21 01:36:29 -0500

    add(player): class

---
+++ jjx_editor/player.py
@@ -0,0 +1,18 @@
+##-----------------------------##
+## Junk Jack X Editor          ##
+## Written By: Ryan Smith      ##
+##-----------------------------##
+## Player Class                ##
+##-----------------------------##
+
+## Imports
+from __future__ import annotations
+
+
+## Classes
+class Player:
+    """"""
+
+    # -Constructor
+    def __init__(self) -> Player:
+        pass
