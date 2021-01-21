7438ea83dc0e383f4648254d45de971cc47d6f11
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-01-21 01:48:17 -0500

    update(player): constructor

--- jjx_editor/player.py
+++ jjx_editor/player.py
@@ -14,5 +14,21 @@ class Player:
     """"""
 
     # -Constructor
-    def __init__(self) -> Player:
-        pass
+    def __init__(
+        self, name: str, gender: bool, skin: int,
+        hair_style: int, hair_color: int, difficulty: int,
+        permadeath: bool, simple_craft: bool, perpetual_time: bool
+    ) -> Player:
+        ''''''
+        # -Character
+        self.name = name
+        self.gender = gender
+        self.skin = skin
+        self.hair_style = hair_style
+        self.hair_color = hair_color
+        # -Gameplay options
+        self.difficulty = difficulty
+        self.permadeath = permadeath
+        self.simple_craft = simple_craft
+        self.perpetual_time = perpetual_time
+        # -Inventory
